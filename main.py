4503c21124837285f066b23bbddd64981f165c85
Thoa Nguyen <kimthoa120494@gmail.com>
2025-11-03 18:39:10 +0700

    Enforce minimum 3 characters for task name , auto-capitalize each word, prevent duplicate task names.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import task_utils
+from task_utils import add_task
 
 if __name__ == "__main__":
     tasks=[]
-    task_utils.add_task(tasks,input("input new task:"))
-    print(tasks)
+    add_task(tasks,"learn code python")
+    add_task(tasks,"learn english")
+    add_task(tasks, "abc")
+    add_task(tasks, "learn ENGLISH")
+    for i,task in enumerate(tasks,start=1):
+        print(f"{i}. {task['name']} - {task['status']}")
--- task_utils.py
+++ task_utils.py
@@ -1,11 +1,16 @@
 def add_task(tasks: list[dict[str,str]],name: str):
     """
         Thêm một task mới vào danh sách.
-        - Yêu cầu: name không được rỗng sau khi strip().
+        - Yêu cầu: name không được rỗng sau khi strip(), tối thiểu 3 ký tự, không được trùng task
         - Trả về: True nếu thêm thành công, False nếu thất bại.
         """
-    name=name.strip()
+    name=name.strip().capitalize()
     if not name:
         return False
-    tasks.append({"name":name})
+    if len(name) < 3:
+        return False
+    for task in tasks:
+        if task["name"].lower() == name.lower():
+            return False
+    tasks.append({"name":name,"status":"To do"})
     return True
