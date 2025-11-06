abc2c2132425bd99f17f88c37185893d43b3d47a
Thoa Nguyen <kimthoa120494@gmail.com>
2025-11-06 22:28:17 +0700

    write task to file + load task from file

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from task_utils import add_task, show_task, delete_task
+from task_utils import add_task, show_task, delete_task, load_task,save_tasks
 
 if __name__ == "__main__":
+
     tasks=[]
     add_task(tasks,"learn code python")
     add_task(tasks,"learn english")
     add_task(tasks, "API Testing")
-    show_task(tasks)
+    show_task(load_task())
     while True:
         try:
             index= int(input("Nhập index cần xoá:"))
